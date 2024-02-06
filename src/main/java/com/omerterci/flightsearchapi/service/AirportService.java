48ba833fd99f139cd4024e51f18d360442401ff7
Ömer Terci <omer.terci@ozu.edu.tr>
2024-02-06 14:04:17 +0300

    Airport and Flight Service added
    AirportService for Airport entity CRUD operations.
    FlightService with flight search functionality.

---
+++ src/main/java/com/omerterci/flightsearchapi/service/AirportService.java
@@ -0,0 +1,36 @@
+package com.omerterci.flightsearchapi.service;
+
+import com.omerterci.flightsearchapi.model.Airport;
+import com.omerterci.flightsearchapi.repository.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AirportService {
+
+    private final AirportRepository airportRepository;
+
+    @Autowired
+    public AirportService(AirportRepository airportRepository) {
+        this.airportRepository = airportRepository;
+    }
+
+    public Airport saveAirport(Airport airport) {
+        return airportRepository.save(airport);
+    }
+
+    public List<Airport> findAllAirports() {
+        return airportRepository.findAll();
+    }
+
+    public Optional<Airport> findAirportById(Long id) {
+        return airportRepository.findById(id);
+    }
+
+    public void deleteAirport(Long id) {
+        airportRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/omerterci/flightsearchapi/service/FlightService.java
@@ -0,0 +1,32 @@
+package com.omerterci.flightsearchapi.service;
+
+import com.omerterci.flightsearchapi.model.Flight;
+import com.omerterci.flightsearchapi.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    private final FlightRepository flightRepository;
+
+    @Autowired
+    public FlightService(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    public List<Flight> searchFlights(Long departureAirportId, Long arrivalAirportId, LocalDateTime departureDateTime, LocalDateTime returnDateTime) {
+        if(returnDateTime == null) {
+            // Tek yönlü uçuş araması
+            return flightRepository.findByDepartureAirportIdAndArrivalAirportIdAndDepartureDateTimeBetween(
+                    departureAirportId, arrivalAirportId, departureDateTime, departureDateTime.plusDays(1));
+        } else {
+            // Çift yönlü uçuş araması, dönüş tarihi dahil
+            return flightRepository.findByDepartureAirportIdAndArrivalAirportIdAndDepartureDateTimeBetween(
+                    departureAirportId, arrivalAirportId, departureDateTime, returnDateTime);
+        }
+    }
+}
